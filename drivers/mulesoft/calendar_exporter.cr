84fb35e277391e30a307d34b84bb0b82f2b4d8c6
William Le <will@place.technology>
2021-03-31 20:33:49 +0800

    feat(mulesoft/exporter): add untested Calendar exporter

---
+++ drivers/mulesoft/calendar_exporter.cr
@@ -0,0 +1,75 @@
+module MuleSoft; end
+
+require "./models"
+require "place_calendar"
+
+class MuleSoft::CalendarExporter < PlaceOS::Driver
+  descriptive_name "MuleSoft Bookings to Calendar Events Exporter"
+  generic_name :Bookings
+  description %(Retrieves and creates bookings using the MuleSoft API)
+
+  default_settings({
+    time_zone:          "Australia/Sydney"
+  })
+
+
+  accessor calendar : Calendar_1
+
+
+  @time_zone : Time::Location = Time::Location.load("Australia/Sydney")
+  @bookings : Array(Booking) = [] of Booking
+  @events   : Array(PlaceCalendar::Event)   = [] of PlaceCalendar::Event
+  
+  # An array of Attendee that has only the system (room) email address. Generally static
+  @just_this_system : Array(PlaceCalendar::Event::Attendee) = {
+    "name":  system.display_name || system.name,
+    "email": system.email.not_nil!
+  }
+
+  def on_load
+    on_update
+  end
+
+  def on_update
+    subscriptions.clear
+    subscription = system.subscribe(:Bookings_1, :bookings) do |subscription, mulesoft_bookings|
+      # values are always raw JSON strings
+      @bookings = Array(Booking).from_json(mulesoft_bookings)
+      @bookings.each { |b| export_booking(b) }
+    end
+  end
+
+  def status()
+    "WIP"
+  end
+
+  protected def export_booking(booking : Booking)
+    event = booking.to_placeos
+    
+    existing_events_json = calendar.list_events(
+      calendar_id:  system.email.not_nil!,
+      period_start: event.event_start,
+      period_end:   event.event_end
+    )
+    existing_events = Array(Event).from_json(existing_events_json)
+
+    calendar.create_event(
+      title:        event.title,
+      event_start:  event.event_start,
+      event_end:    event.event_end,
+      description:  event.body,
+      user_id:      system.email.not_nil!,
+      attendees:    @just_this_system
+    ) unless event_already_exists?(event, existing_events)
+  end
+
+  protected def event_already_exists?(new_event : PlaceCalendar::Event, existing_events : Array(PlaceCalendar::Event))
+    existing_events.each do |existing_event|
+      return true if new_event.title       == existing_event.title && 
+                     new_event.event_start == existing_event.event_start &&
+                     new_event.event_end   == existing_event.event_end
+    end
+    false
+  end
+
+end
