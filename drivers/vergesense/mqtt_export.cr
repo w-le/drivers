c7aaf89c584109af1932d6390ac652377da1ebdc
William Le <will@place.tech>
2021-08-30 15:26:09 +0800

    fix(vergesense/mqtt): publish to mqtt sequentially

--- drivers/vergesense/mqtt_export.cr
+++ drivers/vergesense/mqtt_export.cr
@@ -56,7 +56,7 @@ class Vergesense::MqttExport < PlaceOS::Driver
     changed_spaces.each do |s|
       topic = [ @mqtt_root_topic, s.building_ref_id, "-", s.floor_ref_id, ".", s.space_type, ".", s.space_ref_id, ".", "count" ].join
       payload = s.people ? (s.people.not_nil!.count || 0) : 0  # There must be a neater way to do this
-      mqtt.publish(topic, payload.to_s)
+      mqtt.publish(topic, payload.to_s).get
       logger.debug { "Published #{payload} to #{topic}" } if @debug
     end
     @previous_vs_spaces = vergesense_floor.spaces
