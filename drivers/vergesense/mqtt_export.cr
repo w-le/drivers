33f9e17426d06e79f442bed57e589843da66651f
William Le <will@place.tech>
2021-09-20 14:36:22 +0800

    fix(vergesense/mqtt_export): subscription count

--- drivers/vergesense/mqtt_export.cr
+++ drivers/vergesense/mqtt_export.cr
@@ -1,27 +1,25 @@
-
 require "./models"
 
 class Vergesense::MqttExport < PlaceOS::Driver
-
   descriptive_name "Vergesense to MQTT Exporter"
   generic_name :VergesenseToMQTT
   description %(Export Vergesense people count data to an MQTT consumer)
 
   accessor vergesense : Vergesense_1
   accessor mqtt : GenericMQTT_1
-  
+
   default_settings({
-    mqtt_root_topic: "/t/root-topic/",
+    mqtt_root_topic:  "/t/root-topic/",
     floors_to_export: [
-      "vergesense_building_id-floor_id"
+      "vergesense_building_id-floor_id",
     ],
-    debug: false
+    debug: false,
   })
 
   @mqtt_root_topic : String = "/t/root-topic/"
   @floors_to_export : Array(String) = [] of String
   @debug : Bool = false
-  
+
   @subscriptions : Int32 = 0
   @previous_counts = Hash(String, UInt32).new
 
@@ -30,24 +28,24 @@ class Vergesense::MqttExport < PlaceOS::Driver
   end
 
   def on_update
-    @mqtt_root_topic  = setting(String, :mqtt_root_topic) || "/t/root-topic"
+    @mqtt_root_topic = setting(String, :mqtt_root_topic) || "/t/root-topic"
     @floors_to_export = setting(Array(String), :floors_to_export) || [] of String
     @debug = setting(Bool, :debug) || false
 
     subscriptions.clear
     @subscriptions = 0
-    @floors_to_export.each do |floor|  
+    @floors_to_export.each do |floor|
       system.subscribe(:Vergesense_1, floor) do |_subscription, vergesense_floor_json|
         vergesense_to_mqtt(Floor.from_json(vergesense_floor_json))
-        @subscriptions += 1
       end
+      @subscriptions += 1
     end
   end
 
   def inspect_state
     {
       vergesense_subscriptions: @subscriptions,
-      people_counts: @previous_counts
+      people_counts:            @previous_counts,
     }
   end
 
@@ -55,10 +53,10 @@ class Vergesense::MqttExport < PlaceOS::Driver
     # Determine which spaces have had their people count change
     changed_spaces = vergesense_floor.spaces.reject { |s| s.people.try &.count == @previous_counts[s.space_ref_id]? }
     logger.debug { "#{changed_spaces.size}/#{vergesense_floor.spaces.size} spaces have changed" } if @debug
-    # Publish the new values 
+    # Publish the new values
     changed_spaces.each do |s|
-      space_id = s.space_ref_id.gsub(/[ \/]/,"")
-      topic = [ @mqtt_root_topic, s.building_ref_id, "-", s.floor_ref_id, ".", s.space_type, ".", space_id, ".", "count" ].join.downcase
+      space_id = s.space_ref_id.gsub(/[ \/]/, "")
+      topic = [@mqtt_root_topic, s.building_ref_id, "-", s.floor_ref_id, ".", s.space_type, ".", space_id, ".", "count"].join.downcase
       # Store the current value, for comparison next time
       @previous_counts[space_id] = payload = s.people.try &.count || 0_u32
       mqtt.publish(topic, payload.to_s).get
